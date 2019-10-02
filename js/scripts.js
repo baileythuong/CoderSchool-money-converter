3adf3e6b888d06f7041b8fe4f2b3564e33b918f0
Bailey Nguyen <baileythuong@gmail.com>
2019-10-02 13:05:22 +0700

    added coin change

--- js/scripts.js
+++ js/scripts.js
@@ -31,18 +31,18 @@
 //   }
 // };
 
-// function exchangedCurrency() {
-//   let from = document.getElementById("from").value;
-//   let to = document.getElementById("to").value;
-//   let value = document.getElementById("value").value;
+function exchangedCurrency() {
+  let from = document.getElementById("from").value;
+  let to = document.getElementById("to").value;
+  let value = document.getElementById("value").value;
 
-//   const moneyValue = exchangeRates[from][to] * value;
-//   const formattedValue = formatCurrency(to, moneyValue);
-//   return formattedValue;
-// }
+  const moneyValue = exchangeRates[from][to] * value;
+  const formattedValue = formatCurrency(to, moneyValue);
+  return formattedValue;
+}
 
 document.getElementById("convert").addEventListener("click", function() {
-    convertCurrency();
+  convertCurrency();
 });
 
 function formatCurrency(type, value) {
@@ -54,20 +54,49 @@ function formatCurrency(type, value) {
 }
 
 async function getConvertedValue(apiCurrency, amount) {
-    let url = "https://free.currconv.com/api/v7/convert?q=" + apiCurrency + "&compact=ultra&apiKey=8cc0cdd2fa20cf1364ae";
-    let jsonObject = await fetch(url);
-    let result = await jsonObject.json();
-    console.log(apiCurrency, result);
-    
-    return result[apiCurrency] * amount;
+  let url =
+    "https://free.currconv.com/api/v7/convert?q=" +
+    apiCurrency +
+    "&compact=ultra&apiKey=8cc0cdd2fa20cf1364ae";
+  let jsonObject = await fetch(url);
+  let result = await jsonObject.json();
+  console.log(apiCurrency, result);
+
+  return result[apiCurrency] * amount;
 }
 
 async function convertCurrency() {
-let from = document.getElementById("from").value;
-let to = document.getElementById("to").value;
-let apiCurrency = (from + "_" + to).toUpperCase();
-let amount = document.getElementById("value").value;
+  let from = document.getElementById("from").value;
+  let to = document.getElementById("to").value;
+  let apiCurrency = (from + "_" + to).toUpperCase();
+  let amount = document.getElementById("value").value;
 
-const convertedRate = await getConvertedValue(apiCurrency, amount);
-document.getElementById("result").innerHTML = formatCurrency (to, convertedRate);
+  const convertedRate = await getConvertedValue(apiCurrency, amount);
+  document.getElementById("result").innerHTML = formatCurrency(
+    to,
+    convertedRate
+  );
+  changeToCoins(convertedRate);
+}
+
+let coins = [500000, 200000, 100000, 50000, 20000, 10000, 5000, 2000, 1000];
+let money = [];
+
+function changeToCoins(amount) {
+  // let amount = document.getElementById("value").value;
+  for (let i = 0; i < coins.length; i++) {
+    let currentAmount = coins[i];
+    money[i] = Math.floor(amount / currentAmount);
+    if (money[i] > 0) {
+      amount = amount - currentAmount * money[i];
+    }
+  }
+
+  let coinsMessage = "";
+
+  for (let i = 0; i < money.length; i++) {
+    coinsMessage += `<li>${coins[i]} * ${money[i]}</li>`;
+  }
+  console.log(coinsMessage);
+  document.getElementById("notes").innerHTML = coinsMessage;
 }
